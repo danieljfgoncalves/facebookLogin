13163b960cf32141e2d344d93a656bc354f5c6a4
Daniel Goncalves <danieljfgoncalves@gmail.com>
2015-05-13 12:06:58 -0400

    Initial Commit

---
+++ TodayPhoto/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TodayPhoto
+//
+//  Created by Daniel Goncalves on 2015-05-13.
+//  Copyright (c) 2015 Daniel J Goncalves. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
